74b72ecc7aa2e97b8f187c0960408fee16773c4f
Henrique Rubido <henriquerubido1@gmail.com>
2022-11-20 16:49:22 -0300

    setup: add pages folder

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen';
-import PokemonsScreen from './PokemonsScreen';
-import PokemonScreen from './PokemonScreen';
-import FavoritosScreen from './FavoritosScreen';
+import LoginScreen from './pages/LoginScreen';
+import PokemonsScreen from './pages/PokemonsScreen';
+import PokemonScreen from './pages/PokemonScreen';
+import FavoritosScreen from './pages/FavoritosScreen';
 
 export default function App() {
 
