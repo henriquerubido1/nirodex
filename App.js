a6a0bb4ed33b03d31f2f6bc727b851762af0a497
DiegoManucci <diegobizzotto2003@gmail.com>
2022-11-20 16:39:19 -0300

    router criado

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './LoginScreen';
+import PokemonsScreen from './PokemonsScreen';
+import PokemonScreen from './PokemonScreen';
+import FavoritosScreen from './FavoritosScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Login' }}
+        />
+        <Stack.Screen
+          name="Pokemons"
+          component={PokemonsScreen}
+          options={{ title: 'Pokemons' }}
+        />
+        <Stack.Screen
+          name="Favoritos"
+          component={FavoritosScreen}
+          options={{ title: 'Favoritos' }}
+        />
+        <Stack.Screen
+          name="Pokemon"
+          component={PokemonScreen}
+          options={{ title: 'Pokemon' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ FavoritosScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function FavoritosScreen() {
+
+  return (
+    <Text>Olá</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
